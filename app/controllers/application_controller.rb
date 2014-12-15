e8d77051459abc5a1df25182e3defb649ec10c29
dssreddy123 <dssreddy123@gmail.com>
2014-12-15 10:15:45 +0530

    'secondcommit86287'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+before_filter :redirect_to_https
+
+  def redirect_to_https
+    redirect_to "https://example.com#{request.fullpath}" if !request.ssl? && request.host != "localhost"
+  end
 end
+
